2270dd8ccaa9b7233414d740bf3f9585930f998d
marhcouto <marhcouto1101@gmail.com>
2021-11-21 19:35:36 +0000

    Updated 2_15

--- TP-Exercises/2/2_15.hs
+++ TP-Exercises/2/2_15.hs
@@ -1,4 +1,4 @@
 intersperse' :: a -> [a] -> [a]
 intersperse' _ [] = []
 intersperse' _ [a] = [a]
-intersperse' v (x:xs) = [x, v] ++ (intersperse' v xs)
+intersperse' v (x:xs) = x : v : (intersperse' v xs)
