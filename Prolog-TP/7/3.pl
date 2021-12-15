68a95062ead501aa71f15c94fb941ec7d9896cf0
marhcouto <marhcouto1101@gmail.com>
2021-12-15 10:13:27 +0000

    .

---
+++ Prolog-TP/7/3.pl
@@ -0,0 +1,20 @@
+cargo(tecnico, eleuterio).
+cargo(tecnico, juvenaldo).
+cargo(analista, leonilde).
+cargo(analista, marciliano).
+cargo(engenheiro, osvaldo).
+cargo(engenheiro, porfirio).
+cargo(engenheiro, reginaldo).
+cargo(supervisor, sisnando).
+cargo(supervisor_chefe, gertrudes).
+cargo(secretaria_exec, felismina).
+cargo(diretor, asdrubal).
+chefiado_por(tecnico, engenheiro).
+chefiado_por(engenheiro, supervisor).
+chefiado_por(analista, supervisor).
+chefiado_por(supervisor, supervisor_chefe).
+chefiado_por(supervisor_chefe, diretor).
+chefiado_por(secretaria_exec, diretor).
+
+superior(X,Y):- cargo(Z,Y), cargo(T,X), chefiado_por(Z,T).
+superior(X,Y):- cargo(T,X), chefiado_por(Z,T), cargo(Z,N), superior(N,Y).
